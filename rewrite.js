1697aca4243d90a59b46fc4f8639089461440013
q609262096 <60651165+q609262096@users.noreply.github.com>
2021-12-28 19:51:42 +0800

    Update rewrite.js

--- rewrite.js
+++ rewrite.js
@@ -1,6 +1,9 @@
-hostname=api-access.pangolin-sdk-toutiao.com,*.gylm315.cn,
+hostname=api-access.pangolin-sdk-toutiao.com,*.gylm315.cn,api-sport.chenglie.tech
   #高佣
 https://prd.h5.gylm315.cn/custom_api/html5/taskv6/v1/everydaySignIn url script-request-header https://ghproxy.com/https://raw.githubusercontent.com/bluesmallpig/scripts/main/gylm.js
 https://api-access.pangolin-sdk-toutiao.com/api/ad/union/sdk/reward_video/reward/ url script-request-body https://ghproxy.com/https://raw.githubusercontent.com/bluesmallpig/scripts/main/gylm.js
 #美的
 http://mp-prod.smartmidea.+v1/certificate/app/list url script-request-body https://ghproxy.com/https://raw.githubusercontent.com/bluesmallpig/scripts/main/mdmj.js
+#多走路
+api-sport.chenglie.tech/s.+ig url script-request-header https://ghproxy.com/https://raw.githubusercontent.com/bluesmallpig/scripts/main/dzl.js
+api-sport.chenglie.tech/wit.+/app.+ url script-request-body https://ghproxy.com/https://raw.githubusercontent.com/bluesmallpig/scripts/main/dzl.js
