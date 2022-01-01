9e1354cc6cd18d75c4823958a128cd7b18228517
q609262096 <60651165+q609262096@users.noreply.github.com>
2022-01-01 20:37:56 +0800

    Update rewrite.js

--- rewrite.js
+++ rewrite.js
@@ -1,4 +1,4 @@
-hostname=api-access.pangolin-sdk-toutiao.com,*.gylm315.cn,api-sport.chenglie.tech,*.palmestore.com,
+hostname=api-access.pangolin-sdk-toutiao.com,*.gylm315.cn,api-sport.chenglie.tech,*.palmestore.com,*.snssdk.com
   #高佣
 https://prd.h5.gylm315.cn/custom_api/html5/taskv6/v1/everydaySignIn url script-request-header https://ghproxy.com/https://raw.githubusercontent.com/bluesmallpig/scripts/main/gylm.js
 https://api-access.pangolin-sdk-toutiao.com/api/ad/union/sdk/reward_video/reward/ url script-request-body https://ghproxy.com/https://raw.githubusercontent.com/bluesmallpig/scripts/main/gylm.js
