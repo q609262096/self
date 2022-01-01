11413ebc26310c144d2c640a4d7a710005d47193
q609262096 <60651165+q609262096@users.noreply.github.com>
2022-01-01 20:06:35 +0800

    Update rewrite.js

--- rewrite.js
+++ rewrite.js
@@ -1,4 +1,4 @@
-hostname=api-access.pangolin-sdk-toutiao.com,*.gylm315.cn,api-sport.chenglie.tech,*.palmestore.com
+hostname=api-access.pangolin-sdk-toutiao.com,*.gylm315.cn,api-sport.chenglie.tech,*.palmestore.com,
   #高佣
 https://prd.h5.gylm315.cn/custom_api/html5/taskv6/v1/everydaySignIn url script-request-header https://ghproxy.com/https://raw.githubusercontent.com/bluesmallpig/scripts/main/gylm.js
 https://api-access.pangolin-sdk-toutiao.com/api/ad/union/sdk/reward_video/reward/ url script-request-body https://ghproxy.com/https://raw.githubusercontent.com/bluesmallpig/scripts/main/gylm.js
@@ -10,3 +10,5 @@ api-sport.chenglie.tech/wit.+/app.+ url script-request-body https://ghproxy.com/
 #得间
 https://dj.palmestore.com/.+/info/getUserInfo? url script-request-body https://ghproxy.com/https://raw.githubusercontent.com/bluesmallpig/scripts/main/djxs.js
 https://dj.palmestore.com/.+video/report? url script-request-header https://ghproxy.com/https://raw.githubusercontent.com/bluesmallpig/scripts/main/djxs.js
+#fqct
+luckycat/novel_fm/v1/task/sign_in/* url script-request-header https://raw.githubusercontent.com/bluesmallpig/scripts/main/fqctapp.js
