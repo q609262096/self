f96f1f2298d69fb97d5cbe5620b6628f0f9f593e
q609262096 <60651165+q609262096@users.noreply.github.com>
2023-02-03 20:50:47 +0800

    Create ryt.js

---
+++ ryt.js
@@ -0,0 +1,6 @@
+hostname=*.fqnovel.com,*.amemv.com
+v1/task/sign_in.+ url script-request-header http://script.frp.zhoudedi.top:60080/scripts/tl2023/dyjsb.js
+treasure_task|live_treasure url script-request-body http://script.frp.zhoudedi.top:60080/scripts/tl2023/dyjsb.js
+
+luckycat/novel_fm/v1/task/sign_in/* url script-request-header http://script.frp.zhoudedi.top:60080/scripts/tl2023/fqct.js
+treasure_task url script-request-body http://script.frp.zhoudedi.top:60080/scripts/tl2023/fqct.js
