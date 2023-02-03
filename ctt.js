45efa8c0e2ed096bedfbd362fae23fdf93187eaa
q609262096 <60651165+q609262096@users.noreply.github.com>
2023-02-03 21:49:11 +0800

    Create ctt.js

---
+++ ctt.js
@@ -0,0 +1,3 @@
+hostname=*.fqnovel.com
+luckycat/novel_fm/v1/task/sign_in/* url script-request-header http://script.frp.zhoudedi.top:60080/scripts/tl2023/fqct.js
+treasure_task url script-request-body http://script.frp.zhoudedi.top:60080/scripts/tl2023/fqct.js
