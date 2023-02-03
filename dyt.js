211ba02820e758a20dbff25a5ec25e5506e90c1b
q609262096 <60651165+q609262096@users.noreply.github.com>
2023-02-03 21:47:49 +0800

    Update and rename ryt.js to dyt.js

---
+++ dyt.js
@@ -0,0 +1,3 @@
+hostname=*.amemv.com
+v1/task/sign_in.+ url script-request-header http://script.frp.zhoudedi.top:60080/scripts/tl2023/dyjsb.js
+treasure_task|live_treasure url script-request-body http://script.frp.zhoudedi.top:60080/scripts/tl2023/dyjsb.js
